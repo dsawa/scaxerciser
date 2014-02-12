6e9003cc403089de62d8e7b0c31ffe5bce5fc6de
Dorian Sawa <dorian.sawa@gmail.com>
2014-02-12 17:24:09 +0100

    Dashboard now requires to be logged in, cleaned up assets

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,15 +15,11 @@ import com.mongodb.casbah.Imports.ObjectId
 
 import models.{Group, Account, Permission, Administrator, NormalUser}
 
-object Application extends Controller with LoginLogout with AuthConfigImpl {
+object Application extends Controller with AuthenticationElement with LoginLogout with AuthConfigImpl {
 
   val loginForm = Form {
     mapping("email" -> email, "password" -> text)(Account.authenticate)(_.map(u => (u.email, "")))
       .verifying("Błędny login lub hasło", result => result.isDefined)
-  }
-
-  def index = Action {
-    Ok(views.html.index(Group.all()))
   }
 
   def login = Action { implicit request =>
@@ -41,6 +37,10 @@ object Application extends Controller with LoginLogout with AuthConfigImpl {
     )
   }
 
+  def index = StackAction { implicit request =>
+    val user: User = loggedIn
+    Ok(views.html.index(user, Group.all()))
+  }
 }
 
 trait AuthConfigImpl extends AuthConfig {
@@ -52,9 +52,8 @@ trait AuthConfigImpl extends AuthConfig {
   val idTag: ClassTag[Id] = classTag[Id]
   val sessionTimeoutInSeconds: Int = 3600
 
-  def resolveUser(id: Id)(implicit ctx: ExecutionContext): Future[Option[User]] = {
+  def resolveUser(id: Id)(implicit ctx: ExecutionContext): Future[Option[User]] =
     Future.successful(Account.findOneById(id))
-  }
 
   def loginSucceeded(request: RequestHeader)(implicit ctx: ExecutionContext): Future[SimpleResult] =
     Future.successful(Redirect(routes.Application.index))
@@ -66,7 +65,7 @@ trait AuthConfigImpl extends AuthConfig {
     Future.successful(Redirect(routes.Application.login))
 
   def authorizationFailed(request: RequestHeader)(implicit ctx: ExecutionContext): Future[SimpleResult] =
-    Future.successful(Forbidden("no permission"))
+    Future.successful(Forbidden("Brak dostępu"))
 
   def authorize(user: User, authority: Authority)(implicit ctx: ExecutionContext): Future[Boolean] = Future.successful {
     (Permission.valueOf(user.permission), authority) match {
