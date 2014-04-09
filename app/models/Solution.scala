23ad3c98b44db7addaa955b826cd3f421b647aa4
Dorian Sawa <dorian.sawa@gmail.com>
2014-04-09 18:08:22 +0200

    Poprawki do modelu Solution

--- app/models/Solution.scala
+++ app/models/Solution.scala
@@ -12,12 +12,12 @@ case class Solution(@Key("_id") id: ObjectId, userId: ObjectId, assignmentId: Ob
 
 object Solution extends ModelCompanion[Solution, ObjectId] {
   val solutionsCollection = MongoConnection()(DBConfig.solutions("db"))(DBConfig.solutions("collection"))
-  val dao = new SalatDAO[Assignment, ObjectId](collection = solutionsCollection) {}
+  val dao = new SalatDAO[Solution, ObjectId](collection = solutionsCollection) {}
   val gridfs = GridFS(projectsDb)
 
   private lazy val projectsDb = MongoClient(DBConfig.defaultHost, DBConfig.defaultPort)(DBConfig.solutionsProjects("db"))
 
   def all(): List[Solution] = Solution.findAll().toList
 
-  def create(assignment: Assignment): Option[ObjectId] = dao.insert(assignment)
+  def create(solution: Solution): Option[ObjectId] = dao.insert(solution)
 }
