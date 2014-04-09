0405288982703933674152e3351ab86c8078f0af
Dorian Sawa <dorian.sawa@gmail.com>
2014-04-09 20:14:49 +0200

    Fixy

--- app/controllers/Solutions.scala
+++ app/controllers/Solutions.scala
@@ -22,33 +22,38 @@ object Solutions extends Controller {
       request.headers.get("Authorization").map {
         basicAuth =>
           val (email, password) = decodeBasicAuth(basicAuth)
-          val user = Account.authenticate(email, password).getOrElse(Unauthorized)
-          try {
-            val assignmentId = request.body.dataParts("assignmentId").head
-            val assignment = Assignment.findOneById(new ObjectId(assignmentId)).getOrElse {
-              NotFound("Assignment " + assignmentId + " not found.")
-            }
-            val projectFileOpt = request.body.file("projectFile")
+          Account.authenticate(email, password) match {
+            case Some(user) =>
+              try {
+                val assignmentId = request.body.dataParts("assignmentId").head
+                Assignment.findOneById(new ObjectId(assignmentId)) match {
+                  case Some(assignment) =>
+                    val projectFileOpt = request.body.file("projectFile")
 
-            if (projectFileOpt.isDefined) {
-              val tmpProjectFile = new File(Play.application.path + "/tmp/" + projectFileOpt.get.filename)
-              projectFileOpt.get.ref.moveTo(tmpProjectFile, replace = true)
+                    if (projectFileOpt.isDefined) {
+                      val tmpProjectFile = new File(Play.application.path + "/tmp/" + projectFileOpt.get.filename)
+                      projectFileOpt.get.ref.moveTo(tmpProjectFile, replace = true)
 
-              Solution.create(assignment.asInstanceOf[Assignment], user.asInstanceOf[Account], tmpProjectFile) match {
-                case Some(objectId) =>
-                  Future {
-                    Solution.analyze(objectId)
-                  }
-                  tmpProjectFile.delete()
-                  Ok("Solution accepted. Your results should be available in short time.")
-                case None =>
-                  tmpProjectFile.delete()
-                  UnprocessableEntity("There was problem with adding solution.")
+                      Solution.create(assignment, user, tmpProjectFile) match {
+                        case Some(objectId) =>
+                          Future {
+                            Solution.analyze(objectId)
+                          }
+                          tmpProjectFile.delete()
+                          Ok("Solution accepted. Your results should be available in short time.")
+                        case None =>
+                          tmpProjectFile.delete()
+                          UnprocessableEntity("There was problem with adding solution.")
+                      }
+
+                    } else BadRequest("File with solution is required.")
+
+                  case None => NotFound("Assignment " + assignmentId + " not found.")
+                }
+              } catch {
+                case ex: NoSuchElementException => BadRequest("Param assignmentId must by present.")
               }
-
-            } else BadRequest("File with solution is required.")
-          } catch {
-            case ex: NoSuchElementException => BadRequest("Param assignmentId must by present.")
+            case None => Unauthorized
           }
       }.getOrElse(Unauthorized)
   }
