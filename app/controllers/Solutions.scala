a92e60dbd1a98fe7520db587b97eff50c41b69e8
Dorian Sawa <dorian.sawa@gmail.com>
2014-04-09 20:02:39 +0200

    Zapisywanie do bazy danych potrzebnych do przeanalizwoania rozwiazania

--- app/controllers/Solutions.scala
+++ app/controllers/Solutions.scala
@@ -1,7 +1,12 @@
 package controllers
 
+import java.io.File
+import play.api.Play
+import play.api.Play.current
 import play.api.mvc._
-import com.mongodb.casbah.Imports._
+import scala.concurrent.{Future, ExecutionContext}
+import ExecutionContext.Implicits.global
+import com.mongodb.casbah.Imports.ObjectId
 import models._
 
 object Solutions extends Controller {
@@ -17,23 +22,33 @@ object Solutions extends Controller {
       request.headers.get("Authorization").map {
         basicAuth =>
           val (email, password) = decodeBasicAuth(basicAuth)
-          Account.authenticate(email, password) match {
-            case Some(user) =>
-              try {
-                val assignmentId = request.body.dataParts("assignmentId").head
-                Assignment.findOneById(new ObjectId(assignmentId)) match {
-                  case Some(assignment) =>
-                    val projectFile = request.body.file("projectFile").getOrElse {
-                      BadRequest("File with solution is required.")
-                    }
+          val user = Account.authenticate(email, password).getOrElse(Unauthorized)
+          try {
+            val assignmentId = request.body.dataParts("assignmentId").head
+            val assignment = Assignment.findOneById(new ObjectId(assignmentId)).getOrElse {
+              NotFound("Assignment " + assignmentId + " not found.")
+            }
+            val projectFileOpt = request.body.file("projectFile")
 
-                    Ok("Solution accepted. Your results should be available in short time.")
-                  case None => NotFound("Assignment " + assignmentId + " not found.")
-                }
-              } catch {
-                case ex: NoSuchElementException => BadRequest("Param assignmentId must by present.")
+            if (projectFileOpt.isDefined) {
+              val tmpProjectFile = new File(Play.application.path + "/tmp/" + projectFileOpt.get.filename)
+              projectFileOpt.get.ref.moveTo(tmpProjectFile, replace = true)
+
+              Solution.create(assignment.asInstanceOf[Assignment], user.asInstanceOf[Account], tmpProjectFile) match {
+                case Some(objectId) =>
+                  Future {
+                    Solution.analyze(objectId)
+                  }
+                  tmpProjectFile.delete()
+                  Ok("Solution accepted. Your results should be available in short time.")
+                case None =>
+                  tmpProjectFile.delete()
+                  UnprocessableEntity("There was problem with adding solution.")
               }
-            case None => Unauthorized
+
+            } else BadRequest("File with solution is required.")
+          } catch {
+            case ex: NoSuchElementException => BadRequest("Param assignmentId must by present.")
           }
       }.getOrElse(Unauthorized)
   }
--- app/models/Solution.scala
+++ app/models/Solution.scala
@@ -2,22 +2,45 @@ package models
 
 //import com.novus.salat.global._
 
+import java.io.File
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.gridfs.Imports._
 import com.novus.salat.annotations._
 import com.novus.salat.dao.{SalatDAO, ModelCompanion}
 import scaxerciser.context._
 
-case class Solution(@Key("_id") id: ObjectId, userId: ObjectId, assignmentId: ObjectId)
+case class Solution(@Key("_id") id: ObjectId, assignmentId: ObjectId, userId: ObjectId, solutionFileId: ObjectId)
 
 object Solution extends ModelCompanion[Solution, ObjectId] {
   val solutionsCollection = MongoConnection()(DBConfig.solutions("db"))(DBConfig.solutions("collection"))
   val dao = new SalatDAO[Solution, ObjectId](collection = solutionsCollection) {}
-  val gridfs = GridFS(projectsDb)
+  val gridfs = GridFS(solutionFilesDb)
 
-  private lazy val projectsDb = MongoClient(DBConfig.defaultHost, DBConfig.defaultPort)(DBConfig.solutionsProjects("db"))
+  private lazy val solutionFilesDb = MongoClient(DBConfig.defaultHost, DBConfig.defaultPort)(DBConfig.solutionsProjects("db"))
 
   def all(): List[Solution] = Solution.findAll().toList
 
-  def create(solution: Solution): Option[ObjectId] = dao.insert(solution)
+  def create(assignment: Assignment, user: Account, file: File): Option[ObjectId] = {
+    insertFile(file, contentType = "application/java-archive") match {
+      case Some(potentialId) =>
+        val solutionFileId = potentialId.asInstanceOf[ObjectId]
+        dao.insert(Solution(new ObjectId, assignment.id, user.id, solutionFileId))
+      case None => None
+    }
+  }
+
+  // TODO: Wysle request do appki zajmujacej sie testowaniem i zapisaniem wynikow
+  def analyze(id: ObjectId) {
+    Thread.sleep(5000)
+    println("Przetestowano " + id.toString)
+  }
+
+  private def insertFile(file: File, contentType: String): Option[AnyRef] = {
+    gridfs(file) {
+      f =>
+        f.filename = file.getName
+        f.contentType = contentType
+    }
+  }
+
 }
