de5dc038a22ba0c417019a96d191e8f6ff008f49
Dorian Sawa <dorian.sawa@gmail.com>
2014-05-06 17:08:59 +0200

    Stworzenie nowej roli

--- app/models/Permission.scala
+++ app/models/Permission.scala
@@ -3,13 +3,17 @@ package models
 sealed trait Permission
 
 case object Administrator extends Permission
+
+case object Educator extends Permission
+
 case object NormalUser extends Permission
 
 object Permission {
 
   def valueOf(value: String): Permission = value match {
     case "Administrator" => Administrator
-    case "NormalUser"    => NormalUser
+    case "Educator" => Educator
+    case "NormalUser" => NormalUser
     case _ => throw new IllegalArgumentException()
   }
 
